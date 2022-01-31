cc77dfed21d83fc9caef768a3f439d00998eb1d5
Nazmul Ali Biswas <96861468+nazmulalibiswas@users.noreply.github.com>
2022-01-31 10:18:56 -0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int number_1, number_2, number_3;
+    printf ("Enter 1st number");
+    scanf("%d",& number_1);
+    printf ("Enter 2nd number");
+    scanf("%d",& number_2);
+    printf ("Enter 3rd number");
+    scanf("%d",& number_3);
+    printf("The multipies result is %d", number_1*number_2*number_3);
+    return 0;
+}
